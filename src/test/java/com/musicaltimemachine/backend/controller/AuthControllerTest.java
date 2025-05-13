77c8756fa7f4eaa722df8564b41004aad4b50c1f
rworsham <robertworsham01@gmail.com>
2025-05-12 21:57:36 -0400

    Framing out controller test for integration testing

---
+++ src/test/java/com/musicaltimemachine/backend/controller/AuthControllerTest.java
@@ -0,0 +1,17 @@
+package com.musicaltimemachine.backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+}
